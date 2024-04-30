ce50884db0b0019e08851f6670333124043831c3
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2024-04-29 23:58:32 -0300

    Make new modules.

--- src/EngineRuntime/Shared/EngineModule.lua
+++ src/EngineRuntime/Shared/EngineModule.lua
@@ -1,8 +1,23 @@
---- @class BaseEngineModule
-local module: BaseEngineModule = {}
+local moduleConstructor = {}
 
-module.ModuleName = "EngineModule"
+function moduleConstructor.new()
+	--- The base of a module in VEngine, containing the minimum required methods and properties to initialize one.
+	--- @class BaseEngineModule
+	local module: BaseEngineModule = {}
 
-function module:Initialize(env: EngineEnvironmentManager) end
+	--- The name of the engine module.
+	--- @type string
+	module.ModuleName = "EngineModule"
 
-return module
+	--- Runs the initialization routine for an EngineModule.
+	--- @param _ EngineEnvironmentManager The environment manager for VEngine.
+	function module:Initialize(_: EngineEnvironmentManager)
+		print(
+			"This has not been overriden by you, the developer. You should override it to give your module functionality!"
+		)
+	end
+
+	return module
+end
+
+return moduleConstructor
