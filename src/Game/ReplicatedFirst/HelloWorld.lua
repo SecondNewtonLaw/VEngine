927734c535d069af3b42382ff8e1b2d0b44fb2c8
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2024-05-01 15:45:11 -0300

    Remove environment test on ReplicatedFirst, as the environment is not yet loaded.

--- src/Game/ReplicatedFirst/HelloWorld.lua
+++ src/Game/ReplicatedFirst/HelloWorld.lua
@@ -1,12 +1,11 @@
-local module = require(game:GetService("ReplicatedStorage"):WaitForChild("EngineShared"):WaitForChild("EngineModule")).new()
+local module =
+	require(game:GetService("ReplicatedStorage"):WaitForChild("EngineShared"):WaitForChild("EngineModule")).new()
 
 module.ModuleName = "Hello, World!"
 
-function module:Initialize(environmentManager: EngineEnvironmentManager)
-	print("Hello from the pre-initialization routine inside of ReplicatedFirst!")
-	warn("Hello, this is a warning from inside a polluted environment!")
-	error("This is an error from inside a polluted environment!")
-	critical("This is a critical error from the inside of a polluted environment!")
+function module:Initialize(_)
+	-- On ReplicatedFirst. the EngineEnvironmentManager is NOT available!
+	print("HELLO")
 end
 
 return module
