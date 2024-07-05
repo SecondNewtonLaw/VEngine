7bd549bb653a74e783088908aa9f97600594ff9d
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2024-07-05 02:04:37 -0300

    VEngine internals re-structure; rename files to .luau.

--- CustomTypeDefinitions/README.lua
+++
@@ -1,5 +0,0 @@
---[[
-    This file is meant to be used by YOU to note your types!
-
-    This file is included automagically by Roblox LSP to provide you types all over the editor. The main way we achieve types to be useful.
-]]
